cf43bbff03cdbe9cb15f62eda4ec3cc51d666858
Oussama.Miladi <oussama.miladi@esprit.tn>
2026-05-20 00:25:54 +0100

    checking pipeline v 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
+  shellApiUrl: '/api',
+  aiApiUrl: '/api'
 
-  shellApiUrl: 'http://localhost:8089/SS',
+  //shellApiUrl: 'http://localhost:8089/SS',
   //shellApiUrl: 'https://station-sync-render.onrender.com/SS',
-  aiApiUrl: 'http://localhost:8089/SS'
+  //aiApiUrl: 'http://localhost:8089/SS'
 };
